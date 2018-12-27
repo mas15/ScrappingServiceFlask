68158c627dbd11bfe128179e88da6a6546e212ab
Mateusz Stankiewicz <stanek.94@o2.pl>
2018-12-27 12:11:30 +0100

    add initial files and scrapping module

---
+++ app/scrapping.py
@@ -0,0 +1,48 @@
+import mimetypes
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+from utils import make_dir_for_images, save_scrapped_image, save_scrapped_text
+
+
+def scrape_from_site(url, task_id):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    scrape_images(soup, url, task_id)
+    scrape_text(soup, task_id)
+
+
+def is_tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def scrape_text(soup, task_id):
+    texts = soup.findAll(text=True)
+    visible_texts = filter(is_tag_visible, texts)
+    result = "\n".join(t.strip() for t in visible_texts)
+    save_scrapped_text(result, task_id)
+
+
+def scrape_images(soup, base_url, task_id):
+    def _fix_relative_url(img_url):
+        return urljoin(base_url, img_url) if 'http' not in img_url else img_url
+
+    img_tags = soup.find_all('img')
+    img_urls = [img['src'] for img in img_tags]
+    img_urls = [_fix_relative_url(url) for url in img_urls]
+
+    if img_urls:
+        make_dir_for_images(task_id)
+        for i, img_url in enumerate(img_urls):
+            response = requests.get(img_url)
+            content_type = response.headers['content-type']
+            extension = mimetypes.guess_extension(content_type)
+
+            save_scrapped_image(response.content, i, task_id, extension)
---
+++ app/utils.py
@@ -0,0 +1,50 @@
+import os
+import hashlib
+
+DOWNLOAD_DIR = "download"
+
+
+def generate_id(url):
+    hashed_id = hashlib.md5(url.encode('utf-8'))
+    return hashed_id.hexdigest()
+
+
+def _scrapped_text_filename(task_id):
+    return os.path.join(DOWNLOAD_DIR, task_id + ".txt")
+
+
+def read_scrapped_text(task_id):
+    with open(_scrapped_text_filename(task_id), 'r') as f:
+        return f.read()
+
+
+def save_scrapped_text(content, task_id):
+    with open(_scrapped_text_filename(task_id), 'w') as f:
+        f.write(content)
+
+
+def _task_imgs_dir(task_id):
+    return os.path.join(DOWNLOAD_DIR, task_id)
+
+
+def read_scrapped_images(task_id):
+    data_dir = _task_imgs_dir(task_id)
+    if not os.path.exists(data_dir):
+        return []
+    images_paths = [os.path.join(data_dir, file_name) for file_name in os.listdir(data_dir)]
+    images = []
+    for image_path in images_paths:
+        with open(image_path, 'rb') as f:
+            images.append(f.read())
+    return images
+
+
+def make_dir_for_images(task_id):
+    data_dir = _task_imgs_dir(task_id)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+
+def save_scrapped_image(content, i, task_id, ext):
+    with open(os.path.join(_task_imgs_dir(task_id), f"{i}{ext}"), 'wb') as f:
+        f.write(content)
